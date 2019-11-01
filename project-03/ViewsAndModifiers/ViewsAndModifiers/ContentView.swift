1774c093979768b11daf619d20a85ba6689f53a2
Erik Waterham <waterham@gmail.com>
2019-11-01 15:13:57 +0100

    Add day 24 challenge 1, show large blue title.

--- project-03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ project-03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,9 +8,24 @@
 
 import SwiftUI
 
+struct LargeTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func largeTitle() -> some View {
+        self.modifier(LargeTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+        .largeTitle()
     }
 }
 
