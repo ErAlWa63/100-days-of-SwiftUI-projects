7c01487e5c8379130090d835dd64e89a4404446f
Erik Waterham <waterham@gmail.com>
2019-11-08 14:36:48 +0100

    Add day 37, Building a list we can delete from.

---
+++ project-07/iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Erik Waterham on 08/11/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
