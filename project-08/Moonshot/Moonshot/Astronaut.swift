a0caf85bfea5d2b63fb5ec5a4eaaaeb4a3a025fd
Erik Waterham <waterham@gmail.com>
2019-11-13 11:22:29 +0100

    Add day 40, Loading a specific kind of Codable data.

---
+++ project-08/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Erik Waterham on 13/11/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
