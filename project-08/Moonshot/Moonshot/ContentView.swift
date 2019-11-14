87318ccbfcbd15f796f664f179863075075c3ef5
Erik Waterham <waterham@gmail.com>
2019-11-14 14:27:19 +0100

    Add day 42, make a bar button in ContentView that toggles between showing launch dates and showing crew names.

--- project-08/Moonshot/Moonshot/ContentView.swift
+++ project-08/Moonshot/Moonshot/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
 
+    @State var showCrew = false
+
+
     var body: some View {
         NavigationView {
             List(missions) { mission in
@@ -24,13 +27,42 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formattedLaunchDate)
+                        if self.showCrew {
+                            Text(self.crewNames(mission: mission, astronauts: self.astronauts))
+                                .font(.subheadline)
+                        }
+                        else {
+                            Text(mission.formattedLaunchDate)
+                                .font(.subheadline)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showCrew.toggle()
+                }, label: {
+                    Text("Show \(self.showCrew ? "date" : "crew")")
+                })
+            )
         }
     }
+    func crewNames(mission: Mission, astronauts: [Astronaut], separator: Character = "\n") -> String {
+        var crewNames = ""
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                crewNames += match.name + String(separator)
+            }
+            else {
+                fatalError("Crew member \(member.name) not found")
+            }
+        }
+
+        return String(crewNames.dropLast())
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
