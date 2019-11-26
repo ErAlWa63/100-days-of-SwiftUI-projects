ec03c9f493335c895519b3d69fe47b755651bcfd
Erik Waterham <waterham@gmail.com>
2019-11-26 14:12:33 +0100

    Add day 49, validating and disabling forms.

--- project-10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ project-10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,53 +8,29 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State var results = [Result]()
+    @State var username = ""
+    @State var email = ""
 
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .onAppear(perform: loadData)
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-
-                    // everything is good, so we can exit
-                    return
-                }
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
 
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+
+        }
     }
 }
 
