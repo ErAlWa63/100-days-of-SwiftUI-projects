af01fceb11456eba3b9006bddc59a5b3c59a8812
Erik Waterham <waterham@gmail.com>
2019-11-28 12:44:08 +0100

    Add day 52, challenge 2, if our call to placeOrder() fails – for example if there is no internet connection – show an informative alert for the user. To test this, just disable WiFi on your Mac so the simulator has no connection either.

--- project-10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ project-10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -13,6 +13,8 @@ struct CheckoutView: View {
     @State private var confirmationMessage = ""
     @State private var showingConfirmation = false
 
+    @State private var confirmationTitle = ""
+
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -35,12 +37,16 @@ struct CheckoutView: View {
         }
         .navigationBarTitle("Check out", displayMode: .inline)
         .alert(isPresented: $showingConfirmation) {
-            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+            Alert(title: Text(confirmationTitle), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
         }
     }
 
     func placeOrder() {
         guard let encoded = try? JSONEncoder().encode(order) else {
+            confirmationTitle = "Error"
+            confirmationMessage = "Failed to encode order"
+            showingConfirmation = true
+            
             print("Failed to encode order")
             return
         }
@@ -53,13 +59,22 @@ struct CheckoutView: View {
 
         URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else {
+                self.confirmationTitle = "Error"
+                self.confirmationMessage = "No data in response: \(error?.localizedDescription ?? "Unknown error")."
+                self.showingConfirmation = true
+
                 print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
                 return
             }
             if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationTitle = "Thank you!"
                 self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
                 self.showingConfirmation = true
             } else {
+                self.confirmationTitle = "Error"
+                self.confirmationMessage = "Invalid response from server"
+                self.showingConfirmation = true
+
                 print("Invalid response from server")
             }
         }.resume()
