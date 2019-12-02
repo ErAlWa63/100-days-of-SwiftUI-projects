1095ca04df9fea2bc9694ecd3e9fc7ec49554b14
Erik Waterham <waterham@gmail.com>
2019-12-02 09:25:48 +0100

    Add day 55, using an alert to pop a NavigationLink programmatically.

--- project-11/Bookworm/Bookworm/DetailView.swift
+++ project-11/Bookworm/Bookworm/DetailView.swift
@@ -11,14 +11,18 @@ import CoreData
 
 struct DetailView: View {
     let book: Book
-
+    
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 ZStack(alignment: .bottomTrailing) {
                     Image(self.book.genre ?? "Fantasy")
                         .frame(maxWidth: geometry.size.width)
-
+                    
                     Text(self.book.genre?.uppercased() ?? "FANTASY")
                         .font(.caption)
                         .fontWeight(.black)
@@ -31,23 +35,43 @@ struct DetailView: View {
                 Text(self.book.author ?? "Unknown author")
                     .font(.title)
                     .foregroundColor(.secondary)
-
+                
                 Text(self.book.review ?? "No review")
                     .padding()
-
+                
                 RatingView(rating: .constant(Int(self.book.rating)))
                     .font(.largeTitle)
-
+                
                 Spacer()
             }
         }
         .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+            
+            .alert(isPresented: $showingDeleteAlert) {
+                Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                    self.deleteBook()
+                    }, secondaryButton: .cancel()
+                )
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        // uncomment this line if you want to make the deletion permanent
+        // try? self.moc.save()
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-
+    
     static var previews: some View {
         let book = Book(context: moc)
         book.title = "Test book"
@@ -55,7 +79,7 @@ struct DetailView_Previews: PreviewProvider {
         book.genre = "Fantasy"
         book.rating = 4
         book.review = "This was a great book; I really enjoyed it."
-
+        
         return NavigationView {
             DetailView(book: book)
         }
