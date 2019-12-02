d5903418bb2995e3717ad8034d7b36883ad96aec
Erik Waterham <waterham@gmail.com>
2019-12-02 11:10:49 +0100

    Add 56, challenge 2, modify ContentView so that books rated as 1 star have their name shown in red.

--- project-11/Bookworm/Bookworm/ContentView.swift
+++ project-11/Bookworm/Bookworm/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                         
                         VStack(alignment: .leading) {
                             Text(book.title ?? "Unknown Title")
+                                .foregroundColor((book.rating == 1) ? Color.red : Color.black)
                                 .font(.headline)
                             Text(book.author ?? "Unknown Author")
                                 .foregroundColor(.secondary)
