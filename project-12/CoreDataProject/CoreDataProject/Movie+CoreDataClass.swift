5b2a94c7e4d390b709d65501957b96f808493c58
Erik Waterham <waterham@gmail.com>
2019-12-06 22:06:05 +0100

    Add day 57, creating NSManagedObject subclasses.

---
+++ project-12/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Erik Waterham on 06/12/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
