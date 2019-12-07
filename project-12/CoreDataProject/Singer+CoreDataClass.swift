a26cc95a85a4bf09ce3f4143eba2b09cdfa9eb29
Erik Waterham <waterham@gmail.com>
2019-12-07 23:55:07 +0100

    Add day 58, dynamically filtering @FetchRequest with SwiftUI.

---
+++ project-12/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Erik Waterham on 07/12/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
---
+++ project-12/CoreDataProject/Singer+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Singer+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Erik Waterham on 07/12/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Singer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Singer> {
+        return NSFetchRequest<Singer>(entityName: "Singer")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
+
+}
