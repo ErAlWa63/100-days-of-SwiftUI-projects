1330fd283672ae67cc3943785ed948d1703adafa
Erik Waterham <waterham@gmail.com>
2019-12-20 12:40:37 +0100

    Add day 65, building our basic UI.

--- project-13/Instafilter/Instafilter/ContentView.swift
+++ project-13/Instafilter/Instafilter/ContentView.swift
@@ -10,43 +10,53 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-    }
-
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
-    }
-}
-
 struct ContentView: View {
     @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
 
-            Button("Select Image") {
-               self.showingImagePicker = true
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    // select an image
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: self.$filterIntensity)
+                }.padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        // save the picture
+                    }
+                }
             }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
     }
 
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
